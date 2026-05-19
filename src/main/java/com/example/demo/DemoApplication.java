137d99f73bd594aef8996c002d1a0736a74f0440
kanaaa224 <kanaaa224@outlook.com>
2026-05-20 00:47:30 +0900

    Setup SQLite with JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Files.createDirectories(Path.of("storage"));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
